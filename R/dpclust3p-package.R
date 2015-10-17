1ec9bbcad67891ca6c33e5f661519367bce65466
Stefan Dentro <sd11@sanger.ac.uk>
2015-10-17 14:39:16 +0100

    Moving the package main doc file

---
+++ R/dpclust3p-package.R
@@ -0,0 +1,10 @@
+#' @name dpclust3p
+#' @docType package
+#' @title Dirichlet Process Clustering PreProcessing Package
+#' @author Stefan Dentro, \email{sd11@@sanger.ac.uk}
+#' @keywords package
+#' @import VariantAnnotation
+#' @import GenomicRanges
+#' @importFrom IRanges IRanges
+#' @importFrom S4Vectors DataFrame
+NA
