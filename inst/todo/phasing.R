d022b332ac261e7c57b25447f04be1e6984b35a8
Stefan Dentro <sd11@sanger.ac.uk>
2017-09-11 14:41:44 +0100

    Update phasing into runnable script

--- inst/todo/phasing.R
+++ inst/todo/phasing.R
@@ -1,20 +1,110 @@
-# New phasing script to replace the old that used a separate perl script
+# New phasing script to replace the old that used a separate perl script - sd11@sanger.ac.uk
+# now requires Rsamtools v >= 2.14 
 
+args = commandArgs(T)
+samplename = args[1]
+bam_file = args[2]
+vcf_file = args[3]
+outdir = args[4]
+if (length(args) > 4) {
+  fai_file = args[5]
+  ign_file = args[6]
+} else {
+  fai_file = "/lustre/scratch116/casm/cgp/pancancer/reference/genome.fa.fai"
+  ign_file = "/lustre/scratch116/casm/team113/sd11/reference/GenomeFiles/battenberg_ignore/ignore.txt"
+}
 
-# library(Rsamtools)
-# library(VariantAnnotation)
-# 
-# bam_file = "/nfs/users/nfs_c/cgppipe/pancancer/automated/donors/CLLE-ES/151/tumour/ffa976f0-aa60-4867-842e-361afa7d68ac.bam"
-# vcf_file = "/nfs/users/nfs_c/cgppipe/pancancer/workspace/sd11/icgc_pancan_full/analysis_sanger_001/variants/ffa976f0-aa60-4867-842e-361afa7d68ac.svcp_1-0-4.20150202.somatic.snv_mnv.vcf.gz"
+max_distance = 700
+minMapQ = 20
 
-mut_mut_phasing = function(loci_file, phased_file, bam_file, bai_file, max_distance) {
-  # TODO: this must be removed
-  chr.names = c(1:22,"X","Y")
+library(Rsamtools)
+library(VariantAnnotation)
+
+############################################
+# VCF 2 LOCI
+############################################
+#' Parse genome index
+#' 
+#' Convenience function that parses a reference genome index as generated
+#' by samtools index
+#' @param fai_file The index
+#' @return A data frame with columns "chromosome", "length", "offset", "fasta_line_length", "line_blen"
+#' @author sd11
+#' @export
+parseFai = function(fai_file) {
+  fai = read.table(fai_file, header=F, stringsAsFactors=F)
+  colnames(fai) = c("chromosome", "length", "offset", "fasta_line_length", "line_blen")
+  return(fai)
+}
+
+#' Parse chromosomes to ignore file
+#' 
+#' Convenience function that parses an ignore file. This file
+#' is expected to have a single column with just chromosome names
+#' @param ignore_file The file specifying to be ignored chromosomes
+#' @return A data frame with a single column named "chromosome"
+#' @author sd11
+#' @export
+parseIgnore = function(ignore_file) {
+  ign = read.table(ignore_file, header=F, stringsAsFactors=F)
+  colnames(ign) = c("chromosome")
+  return(ign)
+}
+
+#' Transform vcf to loci file
+#' 
+#' Function that dumps the loci of snvs from a series of vcf files into a single loci file
+#' @param vcf_files A vector of vcf files to be considered
+#' @param fai_file Reference genome index
+#' @param ign_file A file with chromosomes to be excluded from consideration
+#' @param outfile Where to store the output
+#' @param dummy_alt_allele The alt allele to store, supply when you want to override what is in the VCF (Default: NA)
+#' @param dummy_ref_allele The reference allele to store, supply when you want to override what is in the VCF (Default: NA)
+#' @author sd11
+#' @export
+vcf2loci = function(vcf_files, fai_file, ign_file, outfile, dummy_alt_allele=NA, dummy_ref_allele=NA) {
+  fai = parseFai(fai_file)
+  ign = parseIgnore(ign_file)
+  allowed_chroms = which(!(fai$chromosome %in% ign$chromosome))
   
+  # Run through each supplied vcf file, collect the loci from each
+  combined.loci = data.frame()
+  for (vcf_file in vcf_files) {
+    vcf.cols = ncol(read.delim(vcf_file, comment.char="#", header=F, stringsAsFactor=F, nrows=1))
+    vcf.cols.default = 10 # vcf file standard contains 10 columns
+    vcf.colClasses = c(NA, NA, "NULL", NA, NA, rep("NULL", 5+(vcf.cols-vcf.cols.default)))
+    vcf.loci = read.delim(vcf_file, comment.char="#", header=F, stringsAsFactor=F, colClasses=vcf.colClasses)
+    colnames(vcf.loci) = c("chromosome", "pos", "ref","alt")
+    vcf.loci.sel = subset(vcf.loci, chromosome %in% fai$chromosome[allowed_chroms])
+    combined.loci = rbind(combined.loci, vcf.loci.sel)
+  }
+  
+  # Override the ref and alt alleles if desired
+  if (!is.na(dummy_alt_allele) & dummy_alt_allele!="NA") {
+    combined.loci$alt = dummy_alt_allele
+  }
+  if (!is.na(dummy_ref_allele) & dummy_ref_allele!="NA") {
+    combined.loci$ref = dummy_ref_allele
+  }
+  
+  # Remove duplicate entries
+  combined.loci = unique(combined.loci)
+  write.table(combined.loci, col.names=F, quote=F, row.names=F, file=outfile, sep="\t")
+}
+
+############################################
+# phasing
+############################################
+
+mut_mut_phasing = function(loci_file, phased_file, bam_file, bai_file, max_distance, minMapQ=20) {
+  # TODO: this must be removed
+  #chr.names = c(1:22,"X","Y")
+  print("START")
   muts <- read.delim(loci_file, header=F, row.names=NULL, stringsAsFactors=F)
   names(muts) = c("CHR","POSITION","WT","MT")
+  chr.names = unique(muts$CHR)
   muts = muts[order(match(muts$CHR,chr.names),muts$POSITION),]
-  
+  print("AFTER READING")
   # Pairwise comparison of all muts, only take those that are close to eachother
   output <- data.frame(Chr = vector(mode="character",length=0), Pos1 = vector(mode="numeric",length=0), Ref1 = vector(mode="character",length=0), Var1 = vector(mode="character",length=0), Pos2 = vector(mode="numeric",length=0), Ref2 = vector(mode="character",length=0), Var2 = vector(mode="character",length=0))
   for(chr in chr.names){
@@ -28,24 +118,25 @@ mut_mut_phasing = function(loci_file, phased_file, bam_file, bai_file, max_dista
       }
     }
   }
-  
+  print("AFTER FIRST FOR")
   
   if (nrow(output) > 0) {
-  
+    print("IN IF")
     count.data = data.frame()
     
     for (i in 1:nrow(output)) {
+      save(file="test.RData", output, i)
       chrom = output$Chr[i]
       dat_pair = data.frame(chrom=rep(output$Chr[i],2), start=c(output$Pos1[i], output$Pos2[i]), end=c(output$Pos1[i], output$Pos2[i]))
       dat_pair = makeGRangesFromDataFrame(dat_pair)
       
       wt_wt = paste(output$Ref1[i], output$Ref2[i], sep="_")
-      wt_mut = paste(output$Var1[i], output$Ref2[i], sep="_")
-      mut_wt = paste(output$Ref1[i], output$Var2[i], sep="_")
+      mut_wt = paste(output$Var1[i], output$Ref2[i], sep="_")
+      wt_mut = paste(output$Ref1[i], output$Var2[i], sep="_")
       mut_mut = paste(output$Var1[i], output$Var2[i], sep="_")
     
       flag = scanBamFlag(isPaired=T, hasUnmappedMate=F, isDuplicate=F, isUnmappedQuery=F) #, isProperPair=T
-      param <- ScanBamParam(which=dat_pair[1,], what=scanBamWhat(), flag=flag)
+      param <- ScanBamParam(which=dat_pair[1,], what=scanBamWhat(), flag=flag, mapqFilter=minMapQ) #, mapqFilter=minMapQ
       bamfile <- BamFile(bam_file, asMates=TRUE)
       bam <- scanBam(bamfile, param=param)[[1]]
     
@@ -59,7 +150,7 @@ mut_mut_phasing = function(loci_file, phased_file, bam_file, bai_file, max_dista
                                                 Num_Mut_WT=allele_pairs[[mut_wt]], 
                                                 Num_WT_Mut=allele_pairs[[wt_mut]]))
     }
-    
+    print("AFTER FIRST FOR in IF")
     # Categorise pairs of mutations
     count.data$phasing = NA
     for(h in 1:nrow(count.data)){
@@ -77,7 +168,7 @@ mut_mut_phasing = function(loci_file, phased_file, bam_file, bai_file, max_dista
         count.data$phasing[h]="subclone-clone"
       }  
     }
-    
+    print("AFTER second FOR in IF")
     write.table(count.data[!is.na(count.data$phasing),],phased_file,sep="\t",quote=F,row.names=F)
   }
 }
@@ -219,8 +310,17 @@ mut_cn_phasing = function(loci_file, phased_file, hap_file, bam_file, bai_file, 
 get_allele_combination_counts = function(bam, dat_pair) {
   alleles = data.frame()
   
-  # alleles = lapply(seq(1, (length(bam$seq)), 2), function(j) {
-  for (j in seq(1, (length(bam$seq)), 2)) {
+  # alleles = lapply(unique(bam$groupid), function(i) {
+  #for (j in seq(1, (length(bam$seq)), 2)) {
+  for (i in unique(bam$groupid)) {
+    if (sum(bam$groupid==i) != 2) {
+      # No two reads in this pair - possibly removed due to mapping quality constraints
+      next
+    }
+    
+    # Take index of first read in pair
+    j = which(bam$groupid==i)[1]
+    
     rel_pos_snv1 = NA
     read_snv1 = NA
     rel_pos_snv2 = NA
@@ -263,12 +363,28 @@ get_allele_combination_counts = function(bam, dat_pair) {
     } else {
       base_snv2 = NA
     }
+    
     alleles = rbind(alleles, data.frame(snv1=base_snv1, snv2=base_snv2, read_1=read_1, read_2=read_2))
-    # data.frame(snv1=base_snv1, snv2=base_snv2, read_1=read_1, read_2=read_2)
   }#)
   
   # alleles = do.call(rbind, alleles)
   alleles_m = na.omit(alleles)
   return(alleles_m)
 }
-  
+
+############################################
+# script
+############################################
+loci_file = file.path(outdir, paste0(samplename, "_loci.txt"))
+phased_file = file.path(outdir, paste0(samplename, "_mutmut_phasing.txt"))
+vcf2loci(list(vcf_file), fai_file, ign_file, loci_file)
+
+mut_mut_phasing(loci_file=loci_file,
+                phased_file=phased_file,
+                bam_file=bam_file,
+                bai_file=paste0(bam_file, ".bai"),
+                max_distance=max_distance,
+                minMapQ=minMapQ)
+
+
+
